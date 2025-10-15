4d4c73ae784759ebbb5b50391ee70f85be7678bb
Ibrahim <g3globalagency@gmail.com>
2025-10-15 16:00:19 +0900

    new main

--- apps/user-ui/src/configs/constants.tsx
+++ apps/user-ui/src/configs/constants.tsx
@@ -1,22 +1,27 @@
 export const navItems: NavItemsTypes[] = [
   {
-    title: "Home",
+    // title: "Home",
+    title: "Нүүр",
     href: "/",
   },
   {
-    title: "Products",
+    // title: "Products",
+    title: "Бүтээгдэхүүнүүд",
     href: "/products",
   },
   {
-    title: "Shops",
+    // title: "Shops",
+    title: "Бүтээгдэхүүнүүд",
     href: "/shops",
   },
   {
-    title: "Offers",
+    // title: "Offers",
+    title: "Хямдрал",
     href: "/offers",
   },
   {
-    title: "Become A Seller",
+    // title: "Become A Seller",
+    title: "Борлуулагчаар бүртгүүлэх",
     href: `${
       process.env.NEXT_PUBLIC_SELLER_SERVER_URI ||
       "https://seller.nomadnet.shop"
