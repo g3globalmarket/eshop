4bcc0a2d81040f06184fee95d1f66c01f0d86484
Ibrahim <g3globalagency@gmail.com>
2025-10-15 17:22:52 +0900

    new ышвуифк

--- apps/admin-ui/src/shared/components/breadcrumbs/index.tsx
+++ apps/admin-ui/src/shared/components/breadcrumbs/index.tsx
@@ -6,7 +6,8 @@ const BreadCrumbs = ({ title }: { title: string }) => {
   return (
     <div className="w-full text-white flex items-center">
       <Link href="/dashboard" className="text-blue-400 cursor-pointer">
-        Dashboard
+        Админ самбар
+        {/* Dashboard */}
       </Link>
       <ChevronRight size={20} className="opacity-[.8]" />
       <span>{title}</span>
