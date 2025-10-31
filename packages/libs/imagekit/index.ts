683c769836b462ac42ed37a481280d197bd32b5a
Ibrahim <g3globalagency@gmail.com>
2025-10-31 14:10:45 +0900

    new store

--- packages/libs/imagekit/index.ts
+++ packages/libs/imagekit/index.ts
@@ -3,5 +3,5 @@ import ImageKit from "imagekit";
 export const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.IMAGEKIT_SECRET_KEY!,
-  urlEndpoint: "https://ik.imagekit.io/shahriarnomadnet",
+  urlEndpoint: "https://ik.imagekit.io/gglobal",
 });
